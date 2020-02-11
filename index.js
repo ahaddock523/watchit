75dc7657211f6f6c8db8fb20b46e5489bae815f2
Andrew Haddock <ahaddock523@gmail.com>
2020-02-10 21:40:39 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+
+const start = debounce(() => {
+	console.log('STARTING USERS PROGRAM');
+}, 100);
+
+chokidar
+	.watch('.')
+	.on('add', start)
+	.on('change', () => {
+		console.log('file changed');
+	})
+	.on('unlink', () => {
+		console.log('file unlinked');
+	});
