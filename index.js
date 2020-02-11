95507b0e5d1d648508c7011080540de60c055673
Andrew Haddock <ahaddock523@gmail.com>
2020-02-11 16:08:38 -0700

    final commit. Added help screen via Caporal library, implemented use of Chokdar library to track file changes, included child_process library to launch file again after change was detected.

--- index.js
+++ index.js
@@ -2,17 +2,39 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const { spawn } = require('child_process');
+const chalk = require('chalk');
 
-const start = debounce(() => {
-	console.log('STARTING USERS PROGRAM');
-}, 100);
+program
+	.version('0.0.1')
+	.argument('[filename]', 'Name of a file to execute')
+	.action(async ({ filename }) => {
+		const name = filename || 'index.js';
+		try {
+			await fsPromises.access(name);
+		} catch (err) {
+			throw new Error(`Could not file the file ${name}`);
+		}
+		let proc;
+		const start = debounce(() => {
+			if (proc) {
+				proc.kill();
+			}
 
-chokidar
-	.watch('.')
-	.on('add', start)
-	.on('change', () => {
-		console.log('file changed');
-	})
-	.on('unlink', () => {
-		console.log('file unlinked');
+			console.log(chalk.greenBright('>>>> Starting Process...'));
+			proc = spawn(
+				'node',
+				[
+					name
+				],
+				{ stdio: 'inherit' }
+			);
+		}, 100);
+
+		chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
 	});
+
+program.parse(process.argv);
